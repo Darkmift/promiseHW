001af8fbd58b57336e9b66553b48f425c7208624
darkmift@gmail.com <darkmift@gmail.com>
2018-09-26 19:11:10 +0300

    added errmsgroup li to alert message div

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cl = console.log.bind(console);
 var patt1 = /[0-9]/g;
-$('.alertContainer').hide();
+var alertContainer = $('.alertContainer');
+alertContainer.hide();
 
 var formObj = {
     self: $('#ContactForm'),
@@ -17,29 +18,44 @@ formObj.email
     .blur((e) => {
         cl(e.target.value);
         !isEmail(e.target.value) ?
-            $('.alertContainer').text("'" + e.target.value + "'" + " is not a valid email").show() :
-            $('.alertContainer').hide();
+            alertContainer.text("'" + e.target.value + "'" + " is not a valid email")
+            .show() :
+            alertContainer.hide();
     });
 
 formObj.self.submit(function(e) {
     e.preventDefault();
     cl('clicked!');
-    var errMsgGroup;
+    var errMsgGroup = $('<ol>');
     formObj.self
         .find("input")
         .each(
             (index, element) => {
-                switch ($(element).attr('name')) {
+                var self = $(element);
+                cl('value: ', self.val());
+                errMsgGroup
+                    .append(
+                        $('<li>')
+                        .text(!self.val() ? String(self.attr('name')) + " must not be empty" :
+                            String(index)
+                        )
+                    );
+
+
+                switch (self.attr('name')) {
                     case 'fullname':
-                        cl('1')
+                        cl('1', self.val())
                         break;
                     case 'email':
-                        cl('2')
+                        cl('2', self.val())
                         break;
                     case 'phone':
-                        cl('3')
+                        cl('3', self.val())
                         break;
                 }
+
             }
         )
+    errMsgGroup.appendTo(alertContainer);
+    alertContainer.show();
 });
